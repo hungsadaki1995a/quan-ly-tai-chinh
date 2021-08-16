3a9e398e74e97cd047f1f6011b9d27cedf59c29a
Hao Pham <h39@e-mall.co.jp>
2021-08-16 17:11:34 +0700

    complete home screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import money from '../assets/money.svg';
 import './Home.scss';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
         return (
                 <div className="home-container">
-                       <img src={money} alt="money background" className="home-background"></img>
+                        <div className="background-wrapper">
+                                <h2 className="font-weight-bold">My Finance</h2>
+                                <img src={money} alt="money background" className="home-background"></img>
+                        </div>
+                        <div className="mt-4">
+                                <div>Ứng dụng quản lý tài chính hiệu quả</div>
+                                <Button className="w-100 mt-4">Đăng nhập</Button>
+                                <Button variant="outline-dark" className="w-100 mt-3">Đăng ký</Button>
+                        </div>
                 </div>
         )
 }
