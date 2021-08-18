6d0fb91b869da6b801a9672ed266330d25b3f381
Hao Pham <h39@e-mall.co.jp>
2021-08-18 15:35:36 +0700

    update login form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,11 +2,22 @@ import login from '../../assets/login.svg';
 import './Login.scss';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { useHistory } from 'react-router-dom';
 
 function Login() {
+        let history = useHistory();
+
+        function back() {
+                history.replace("/");
+        }
         return(
                 <div className="form-login-container">
                         <div className="background-image">
+                                <div className="login-back-icon-wrapper">
+                                        <FontAwesomeIcon icon={faArrowLeft} onClick={back}/>
+                                </div>
                                 <img src={login} alt="money background" className="login-background mt-4"></img>
                         </div>
                         <div className="login-wrapper">
@@ -26,7 +37,7 @@ function Login() {
                                 </Form.Text>
                                 <Form.Control type="password" placeholder="Password" />
                                 </Form.Group>
-                                <Button variant="primary" type="submit" className="w-100">
+                                <Button variant="primary" type="submit" className="w-100 mt-2">
                                 Submit
                                 </Button>
                                 </Form>
